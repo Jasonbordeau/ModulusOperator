38ce10471aa9a1af4952dbfaf0852dfb84eb7844
Jason Bordeau <jason.bordeau.2021@student.lhschools.org>
2020-03-05 11:08:22 -0500

    lastDigitMethodCreated

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[] args) {
         testOddness();
         keepContained();
+        lastDigit();
     }
 
     public static void testOddness() {
@@ -32,6 +33,19 @@ public class Main {
 
        System.out.println("Number contained is: " + (positiveNumber%8+5));
 
+
+    }
+
+    public static void lastDigit(){
+
+        int positiveNumber;
+        Scanner user;
+        user = new Scanner(System.in);
+
+        System.out.println("Enter a Positive Number that is 4 digits long");
+        positiveNumber = user.nextInt();
+
+        System.out.println("The last digit of number is: " + positiveNumber%10);
 
     }
 
